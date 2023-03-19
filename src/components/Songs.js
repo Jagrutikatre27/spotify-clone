859c12948823b154dc2119da0b0c188a849368b2
Jagrutikatre27 <89924590+Jagrutikatre27@users.noreply.github.com>
2023-03-19 21:17:20 +0530

    Update Songs.js

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,103 +1,102 @@
 const Songs = [
-    {
-      id: 1,
-      favourite: false,
-      songName: "Bella Ciao",
-      artist: "El Profesor",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fbellaciao.mp3?alt=media&token=e1dd8233-3521-4574-8957-d51af9a5db5c",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmoneyheist.jpg?alt=media&token=32baf3d7-0ab5-47d5-ba48-554792cea117",
-    },
-    {
-      id:2,
-      favourite: false,
-      songName: "Beast",
-      artist: "Anirudh Ravichander",
-      duration: "03:12",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fbeast.mp3?alt=media&token=ebf29a01-079b-44eb-969a-57ee66a15ee8",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fbeast.jpg?alt=media&token=7cc7c33e-8eac-4f7a-858d-75e75d4b609c",
-    },
-    {
-      id: 3,
-      favourite: false,
-      songName: "Joker - Rock and Roll",
-      artist: "Hildur Guðnadóttir",
-      duration: "01:55",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fjoker.mp3?alt=media&token=9e79751a-726d-4359-8a26-116db925f6ff",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fjoker.jpg?alt=media&token=c3955b3c-7efd-4127-b119-6c6343dbc414",
-    },
-    {
-      id: 4,
-      favourite: false,
-      songName: "I Am A Peaky Blinder",
-      artist: "Otnicka",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fpeaky-blinder.mp3?alt=media&token=2ddb5cfb-f4e0-4604-b77c-a7f63894d3ec",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fpeakyblinder.jpg?alt=media&token=b83fbf86-37ef-47c2-948e-3447a22a2d07",
-    },
-    {
-      id: 5,
-      favourite: false,
-      songName: "Naanga Vera Maari",
-      artist: "Yuvan Shankar Raja",
-      duration: "02:00",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2Fvalimai.mp3?alt=media&token=97c769ab-e2fb-4562-90aa-18c73b320bde",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fvalimai.jpg?alt=media&token=e11b839d-2514-4e1a-aa3a-20db3540a837",
-    },
-    {
-      id: 6,
-      songName: "Your Woman",
-      artist: "White Town",
-      duration: "02:12",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FWhite-Town-Your-Women.mp3?alt=media&token=91817445-98ba-4a99-a4b4-6218f2742eb3",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fwhitetown.jpg?alt=media&token=25e1ec0c-24ec-4a11-a7a7-e55ec1fc50cf",
-    },
-    {
-      id: 7,
-      favourite: false,
-      songName: "Charlie",
-      artist: "Gopi Sundar",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FCharlie.mp3?alt=media&token=5a129114-bb31-4a5a-95b7-1767829df21e",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fcharlie.jpg?alt=media&token=81a1dc08-a9af-4db2-b74d-1f50315f9342",
-    },
-    {
-      id: 8,
-      favourite: false,
-      songName: "Mask Off",
-      artist: "Future",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FFuture-Mask-Off.mp3?alt=media&token=92d8b60f-2bca-406f-914a-40bfb9a9324a",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fmask.jpg?alt=media&token=22f0d7be-74b8-4c0d-849d-b777370e9559",
-    },
-    {
-      id: 9,
-      favourite: false,
-      songName: "Hey Mama",
-      artist: "David Guetta",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FHeyMamaRingtone.mp3?alt=media&token=850148f5-8a15-4f87-995d-711138d5d7a0",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fheymama.jpg?alt=media&token=2d8e8632-8051-4105-9760-3c715d797802",
-    },
-    {
-      id: 10,
-      favourite: false,
-      songName: "Turkish Folk Battle Song",
-      artist: "CVR Toon ",
-      duration: "02:45",
-      song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Ringtone%2FPlevne-turkish.mp3?alt=media&token=72f7e03b-2865-4ffb-a2d1-da4c4557fb80",
-      imgSrc:
-        "https://firebasestorage.googleapis.com/v0/b/spotify-clone-7a2ef.appspot.com/o/Images%2Fturkish.jpg?alt=media&token=1f44ad17-43a8-4e1d-b232-a78b28cb9fe0",
-    },
-  ];
-  
-  export { Songs };
+  {
+    id:1,
+    playpause: false,
+    favourite: false,
+    songName: "Is Qadar",
+    artist: "Darshan Raval",
+    duration: "03:46",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/is-qadar-tulsi-kumar-128-kbps-sound.mp3?alt=media&token=d1963457-7278-48a0-8088-c640fc09cd98",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FIs%20Qadar.jpg?alt=media&token=e68eb64a-bab2-4ee4-8c62-94aac1a997fe",
+  },
+  {
+    id:2,
+    playpause: false,
+    favourite: false,
+    songName: "Aaj Sajeya",
+    artist: "Goldie Sohel",
+    duration: "03:49",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/Aaj%20Sajeya%20Goldie%20Sohel%20320%20Kbps.mp3?alt=media&token=18b821ae-dbe2-4b56-b565-da9842714124",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FAaj-Sajeya.jpg?alt=media&token=9fc02905-cf71-495f-ab99-937e4df7a287",
+  },
+  {
+    id:3,
+    playpause: false,
+    favourite: false,
+    songName: "Baarish Ki Jaaye",
+    artist: "B Praak",
+    duration: "04:11",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/Baarish%20Ki%20Jaaye%20B%20Praak%20320%20Kbps.mp3?alt=media&token=e7c9b452-b0c7-4e3e-8968-c0fa63b1061c",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FBaarish%20ki%20Jaaye.jpg?alt=media&token=facff3a7-fe46-41d2-85c6-eb48b99b33bf",
+  },
+  {
+    id:4,
+    playpause: false,
+    favourite: false,
+    songName: "Lut Gaye",
+    artist: "Jubin Nautiyal",
+    duration: "03:48",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/Lut%20Gaye%20Jubin%20Nautiyal%20320%20Kbps.mp3?alt=media&token=f71fe776-ce05-4d78-bb82-fca92a4d992f",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FLut%20Gaye.jpg?alt=media&token=22e6f96b-b404-41e7-bb98-aab8a0ca3bf0",
+  },
+  {
+    id:5,
+    playpause: false,
+    favourite: false,
+    songName: "Mazaa",
+    artist: "B Praak",
+    duration: "04:23",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/Mazaa%20B%20Praak%20320%20Kbps.mp3?alt=media&token=87c14b90-c2a8-4768-8bff-242d3be90a72",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FMazaa.jpg?alt=media&token=d1f918a3-fcfd-4cd2-b91a-d6cc65caee33",
+  },
+  {
+    id:6,
+    playpause: false,
+    favourite: false,
+    songName: "Panghat",
+    artist: "Asees Kaur, Divya Kumar",
+    duration: "02:59",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/panghat-roohi-128-kbps-sound.mp3?alt=media&token=3b41392f-7012-462c-817e-2049a43b11ea",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FPanghat.jpg?alt=media&token=ce4f628d-439b-4959-adaa-60880a4a8b57",
+  },
+  {
+    id:7,
+    playpause: false,
+    favourite: false,
+    songName: "Thoda Thoda Pyar",
+    artist: "Stebin Ben",
+    duration: "04:04",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/thoda-thoda-pyaar-stebin-ben-128-kbps-sound.mp3?alt=media&token=ebf193ff-477a-40fa-8fc6-26d6d332605f",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FThoda%20Thoda%20Pyaar.jpg?alt=media&token=8fcee551-6c90-40d6-ac70-67ef3997fe11",
+  },
+  {
+    id:8,
+    playpause: false,
+    favourite: false,
+    songName: "Raataan Lambiyan",
+    artist: "Jubin Nautiyal, Asees Kaur",
+    duration: "03:50",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/raataan-lambiyan-shershaah-128-kbps-sound.mp3?alt=media&token=68171db6-681d-4c91-af91-ba069e9626b0",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2FRaataan%20Lambiyan.jpg?alt=media&token=6635f854-cf91-4965-a5f8-dd1379f09793",
+  },
+  {
+    id:9,
+    playpause: false,
+    favourite: false,
+    songName: "Saiyaan Ji",
+    artist: "Yo Yo Honey Singh",
+    duration: "03:25",
+    song: "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/saiyaan-ji-yo-yo-honey-singh-2021-128-kbps.mp3?alt=media&token=955bce06-b413-479c-b629-0f980fd54d1c",
+    imgSrc:
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-93ec3.appspot.com/o/I(mages%2Fsaiyaan%20ji.jpg?alt=media&token=079a6382-101b-4d03-90a1-ce73cec5dfd2",
+  },
+]
+export {Songs};
